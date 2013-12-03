6460cd78b8666b96f0ab190bc3c8646f70873aaa
Grant Spence <gcs278@vt.edu>
2013-12-02 19:47:11 -0500

    Database work

--- src/com/zonesnap/server/Database.java
+++ src/com/zonesnap/server/Database.java
@@ -32,34 +32,17 @@ public class Database {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
 	// Register/Login a user
 	boolean RegisterUser(String username) {
-		// Attempt to insert into database
-		try {
-			String query = "INSERT INTO `users` (`username`,`last_login`) VALUES (?,?)";
-			java.sql.PreparedStatement prepared = connection
-					.prepareStatement(query);
-			prepared.setString(1, username);
-
-			// Set the current date
-			java.util.Date today = new java.util.Date();
-			Timestamp currentTimestamp = new Timestamp(today.getTime());
-			prepared.setTimestamp(2, currentTimestamp);
-
-			prepared.execute(); // Insert
-
-		} catch (MySQLIntegrityConstraintViolationException e) {
+		if (UserExists(username)) {
 			// Means that user is registered
 			// Update his last login
 			String query = "UPDATE `users` SET `last_login` = CURRENT_TIMESTAMP WHERE `username` = ?";
@@ -71,12 +54,56 @@ public class Database {
 			} catch (SQLException e1) {
 				e1.printStackTrace();
 			}
+		} else {
+			// Attempt to insert into database
+			try {
+				String query = "INSERT INTO `users` (`username`,`last_login`) VALUES (?,?)";
+				java.sql.PreparedStatement prepared = connection
+						.prepareStatement(query);
+				prepared.setString(1, username);
+
+				// Set the current date
+				java.util.Date today = new java.util.Date();
+				Timestamp currentTimestamp = new Timestamp(today.getTime());
+				prepared.setTimestamp(2, currentTimestamp);
+
+				prepared.execute(); // Insert
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("Query Failed: " + e.getMessage());
+			}
+		}
+		return true;
+	}
+
+	// Function to determine if user exists
+	boolean UserExists(String username) {
+		boolean exists = false;
+		// See if user exists
+		try {
+			String query = "SELECT * FROM `users` WHERE `username` = ?";
+			java.sql.PreparedStatement prepared = connection
+					.prepareStatement(query);
+			prepared.setString(1, username);
+
+			prepared.execute();
+
+			// Execute
+			ResultSet rs = prepared.executeQuery();
+
+			// Check if we have a result
+			if (rs.next())
+				return true;
+			else
+				return false;
 
 		} catch (SQLException e) {
 			e.printStackTrace();
 			System.out.println("Query Failed: " + e.getMessage());
 		}
-		return true;
+
+		return exists;
 	}
 
 	// Upload a picture for the corresponding user
@@ -198,13 +225,13 @@ public class Database {
 			e.printStackTrace();
 			System.out.println("Query Failed: " + e.getMessage());
 		}
-		
+
 		// Put the data as JSON
 		JSONObject json = new JSONObject();
 		try {
 			json.put("image", new String(imageBytes, "UTF-8"));
 			json.put("title", title);
-			json.put("likes",likes);
+			json.put("likes", likes);
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
@@ -216,7 +243,7 @@ public class Database {
 	@SuppressWarnings("unchecked")
 	String RetrieveUserProfile(String username) {
 		// Variables that we will retreiving
-		int total_likes, zones_crossed;
+		int total_likes = 0, zones_crossed, ranking = 0;
 		JSONObject json = new JSONObject();
 
 		// Query the database
@@ -244,6 +271,27 @@ public class Database {
 			e.printStackTrace();
 			System.out.println("Query Failed: " + e.getMessage());
 		}
+
+		// Query the database for ranking
+		try {
+			// Query and execute
+			String query = "SELECT COUNT(*)+1 AS rank FROM `pictures` where `likes` > ?";
+			PreparedStatement prepared = connection.prepareStatement(query);
+			prepared.setInt(1, total_likes);
+
+			ResultSet rs = prepared.executeQuery();
+
+			// If user exists
+			if (rs.next())
+				ranking = rs.getInt("rank");
+
+			json.put("ranking", ranking);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Query Failed: " + e.getMessage());
+		}
+
 		return json.toJSONString();
 	}
 
@@ -314,12 +362,12 @@ public class Database {
 		}
 		return userID;
 	}
-	
+
 	// Get locations of photos for google map
 	List<String> GetPhotoLocations() {
 		List<String> coorList = new ArrayList<String>();
 		try {
-			String query= "SELECT  `idpictures`,`latitude`,`longitude` FROM  `pictures`";
+			String query = "SELECT  `idpictures`,`latitude`,`longitude`,`title` FROM  `pictures`";
 
 			PreparedStatement statement = connection.prepareStatement(query);
 
@@ -328,12 +376,13 @@ public class Database {
 			// Get usernames
 			while (rs.next()) {
 				int id = rs.getInt("idpictures");
-				double latitude =rs.getDouble("latitude");
+				double latitude = rs.getDouble("latitude");
 				double longitude = rs.getDouble("longitude");
-				String data = id + ","+String.valueOf(latitude) + "," + String.valueOf(longitude);
+				String title = rs.getString("title");
+				String data = id + "," + String.valueOf(latitude) + ","
+						+ String.valueOf(longitude) + "," + title;
 				coorList.add(data);
 			}
-			
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -342,7 +391,7 @@ public class Database {
 		}
 		return coorList;
 	}
-	
+
 	// Get the list of usernames
 	List<Integer> GetPhotoIDs(double latitude, double longitude, String order) {
 		List<Integer> photoIDs = new ArrayList<Integer>();
@@ -405,7 +454,34 @@ public class Database {
 		}
 		return photoIDs;
 	}
-	
+
+	// Get the list of personal pictures
+	List<Integer> GetPersonalPhotos(String username) {
+		List<Integer> photoIDs = new ArrayList<Integer>();
+
+		try {
+			String query = "SELECT  `idpictures` FROM  `pictures` LEFT JOIN `users` ON pictures.users_id = users.id WHERE `username` = ? ORDER BY `date` DESC";
+
+			PreparedStatement statement = connection.prepareStatement(query);
+			statement.setString(1, username);
+
+			ResultSet rs = statement.executeQuery();
+
+			// Get usernames
+			while (rs.next()) {
+				int photo = rs.getInt("idpictures");
+				if (!photoIDs.contains(photo)) {
+					photoIDs.add(photo);
+				}
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Query Failed: " + e.getMessage());
+		}
+		return photoIDs;
+	}
+
 	// Get User ID from string username
 	private int getUserId(String username) {
 		int userId = 0;
@@ -431,7 +507,8 @@ public class Database {
 	}
 
 	// Update that user has crossed more zones
-	boolean UpdateZonesCrossed(String username, double latitude, double longitude) {
+	boolean UpdateZonesCrossed(String username, double latitude,
+			double longitude) {
 		int zoneID = LocateZone(latitude, longitude);
 		if (zoneID == -1) {
 			CreateZone(latitude, longitude);
--- src/com/zonesnap/server/ProfileData.java
+++ src/com/zonesnap/server/ProfileData.java
@@ -1,10 +1,16 @@
 package com.zonesnap.server;
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 public class ProfileData extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -14,17 +20,34 @@ public class ProfileData extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		Database database = new Database();
-		
+
 		// Determine type of request
 		String requestType = req.getParameter("type");
 		String jsonData = null;
-		
+		// Return the user's profile data
+		String username = req.getParameter("user");
+		if (username == null) {
+			resp.getWriter().println("Unknown Request");
+			return;
+		}
 		if (requestType.equalsIgnoreCase(new String("profile"))) {
-			// Return the user's profile data
-			String email = req.getParameter("user");
-			jsonData = database.RetrieveUserProfile(email);
+			jsonData = database.RetrieveUserProfile(username);
+		} else if (requestType.equalsIgnoreCase(new String("pics"))) {
+			// Return list of photoIDs
+			List<Integer> photoIDs = database.GetPersonalPhotos(username);
+			JSONObject json = new JSONObject();
+			JSONArray jsonArray = new JSONArray();
+			for (Iterator<Integer> i = photoIDs.iterator(); i.hasNext();) {
+				String item = i.next().toString();
+				jsonArray.add(item);
+			}
+			
+			// Send data
+			json.put("photoIDs", jsonArray);
+			jsonData = json.toString();
 		} else {
 			resp.getWriter().println("Unknown Request");
+			return;
 		}
 
 		resp.setContentType("text/plaintext");
